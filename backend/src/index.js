ee74f6264e2de7406a9e1874b999359de043c490
Pratham Sharma <sharmapratham208@gmail.com>
2025-04-28 11:55:29 +0530

    index.js deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,13 +23,25 @@ app.use(cors({
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+  app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+    app.get("/login", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+    app.get("/signup", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+    app.get("/profile", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+    app.get("/settings", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+  }
 
 server.listen(PORT , () => {
     console.log("Server is Running om port PORT:" + PORT);
